37c3001633b24fc0ad504382cdb658d3efb29ba4
joanjajas <joanbeltranperis@gmail.com>
2022-06-23 02:09:08 +0200

    install_rust.sh script error fixed

--- scripts/install_rust.sh
+++ scripts/install_rust.sh
@@ -1,5 +1,5 @@
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 curl -L https://github.com/rust-analyzer/rust-analyzer/releases/latest/download/rust-analyzer-x86_64-unknown-linux-gnu.gz | gunzip -c - > ~/.config/cargo/bin/rust-analyzer
 chmod +x ~/.config/cargo/bin/rust-analyzer
-rm -rf ~.zshenv
-rm -rf ~.profile
+rm -rf ~/.zshenv
+rm -rf ~/.profile
