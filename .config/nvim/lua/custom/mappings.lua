c053ff6bb193464bb4de892869a7d470b2a437db
Joanjajas <joanbeltranperis@gmail.com>
2023-01-22 19:00:56 +0100

    nvim: migrated to neovide

--- .config/nvim/lua/custom/mappings.lua
+++ .config/nvim/lua/custom/mappings.lua
@@ -4,18 +4,25 @@ M.general = {
   n = {
     ["<leader>s"] = { ":w<CR>", "save file" },
     ["<BS>"] = { ":q!<CR>", "close file without saving" },
-    ["{{"] = { ":<Esc>A {}<Left><CR><esc>O" },
+    ["{{"] = { "A {}<Left><CR><Rsc>O" },
     [";;"] = { "A;<Esc>" },
+    ["<D-v>"] = { '"+p' },
   },
 
   i = {
     ["kj"] = { "<Esc>" },
     ["{{"] = { "<Esc>A {}<Left><CR><esc>O" },
     [";;"] = { "<Esc>A;" },
+    ["<D-v>"] = { "<C-r>*" },
   },
 
   t = {
     ["kj"] = { "<C-\\><C-n>" },
+    ["<D-v>"] = { '<C-\\><C-n>"+pi' },
+  },
+
+  c = {
+    ["<D-v>"] = { "<C-r>+" },
   },
 }
 
