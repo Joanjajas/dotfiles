a7d444f230a5e65be683e8592c496d90442e999c
joanjajas <joanbeltranperis@gmail.com>
2023-11-03 21:19:24 +0100

    fixed fzf git status script

--- scripts/fzf/open_git.sh
+++ scripts/fzf/open_git.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-selected_file=$(git status --porcelain | awk '{print $2}' | fzf)
+selected_file=$(git status --porcelain | grep -E '^(M|M | M|\?\?)' | fzf)
 
 if [ -n "$selected_file" ]; then
 	nvim "$HOME/$selected_file"
