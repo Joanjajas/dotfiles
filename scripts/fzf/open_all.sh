db291d2d5e81e52918855e05e1e845dbfbd1376b
joanjajas <joanbeltranperis@gmail.com>
2023-11-10 13:44:33 +0100

    updated open_all scripts

--- scripts/fzf/open_all.sh
+++ scripts/fzf/open_all.sh
@@ -8,13 +8,16 @@ selected_file=$(
 		-E .vscode \
 		-E .git \
 		-E .cache \
-		-E .config/cargo/registry \
+		-E .config/cargo \
 		-E .config/rustup \
 		-E Public \
 		-E Pictures \
 		-E Music \
 		-E Movies \
-		-E .DS_Store |
+		-E .DS_Store \
+		-E target \
+		-E .local \
+		-E .npm |
 		fzf
 )
 selected_dir=$(dirname "$selected_file")
